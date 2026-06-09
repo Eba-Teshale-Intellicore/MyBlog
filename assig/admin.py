2f631f9cebd706580eb423f3e6503229e58f63d8
Eba Teshale <ebateshale40@gmail.com>
2026-06-09 11:49:39 +0300

    assig admin

---
+++ assig/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from assig.models import About
+
+# Register your models here.
+
+admin.site.register(About)
---
+++ assig/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AssigConfig(AppConfig):
+    name = 'assig'

abdcd2578b550bb5fc796da635dd2d8170e5ed88
Eba Teshale <ebateshale40@gmail.com>
2026-06-10 00:32:19 +0300

    logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path('blogs/search/', Blog_views.search, name='search'),
     path('register/', views.register, name='sregister'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

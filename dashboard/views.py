c65ec30a76eb736066eb7481af52cfdad4a2a2c1
Eba Teshale <ebateshale40@gmail.com>
2026-06-10 02:00:46 +0300

    svk

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashb(request):
-  return render(request, 'dashboard/dashboard.html')
+  return render(request, 'dashboard/dashboard.html') 
